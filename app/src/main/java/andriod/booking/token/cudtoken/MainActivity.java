e0343c925e485ee66e658962c394507ca14ed289
ashamsu12 <shamsudeenabubakar59@gmail.com>
2018-05-25 13:03:53 +0100

    unstyled - simple navigation

--- app/src/main/java/andriod/booking/token/cudtoken/MainActivity.java
+++ app/src/main/java/andriod/booking/token/cudtoken/MainActivity.java
@@ -1,30 +1,122 @@
 package andriod.booking.token.cudtoken;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.support.v7.app.ActionBar;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import fragments.*;
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    Context cn = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Home Page");
-        Button studentButton, staffButton;
-        studentButton = (Button) findViewById(R.id.studentbutton);
-        staffButton  = (Button) findViewById(R.id.staffbutton);
-        final Context context = this;
-        studentButton.setOnClickListener(new View.OnClickListener() {
-                                             @Override
-                                             public void onClick(View v) {
-                                                 Intent intent = new Intent(context,  ServiceActivity.class);
-                                                 startActivity(intent);
-                                             }
-                                         }
-        );
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setTitle("CUD Token App");
+
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        viewPager.setOffscreenPageLimit(3);
+
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs_et);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+        final Activity act = this;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_optionbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_help) {
+            return true;
+        }
+        if (id == R.id.menu_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        MainActivity.ViewPagerAdapter adapter = new MainActivity.ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), "Home");
+        adapter.addFragment(new AboutFragment(), "About Us");
+        adapter.addFragment(new ContactFragment(), "Contact Us");
+
+
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
---
+++ app/src/main/java/fragments/HomeFragment.java
@@ -0,0 +1,45 @@
+package fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import andriod.booking.token.cudtoken.LoginActivity;
+import andriod.booking.token.cudtoken.MainActivity;
+import andriod.booking.token.cudtoken.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        Button studentBtn = (Button) rootView.findViewById(R.id.student_button);
+
+        studentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( getActivity(), LoginActivity.class));
+            }
+        });
+        return rootView ;
+    }
+
+
+
+}
